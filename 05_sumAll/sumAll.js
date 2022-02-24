97367700f863b80d3c2c32f78af136155d30f6fb
Tommy White <tbw2.mail@gmail.com>
2022-02-24 13:18:06 -0700

    Cleanup code for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,29 +3,24 @@ const sumAll = function (lowEnd, highEnd) {
   let highEndParam = highEnd;
   let numList = [];
 
-  if (lowEndParam < 0 || highEndParam < 0) {
+  if (lowEndParam < 0 || highEndParam < 0) return 'ERROR';
+  if (typeof lowEndParam !== 'number' || typeof highEndParam !== 'number')
     return 'ERROR';
-  } else if (typeof lowEndParam !== 'number' || typeof highEndParam !== 'number') {
-    return 'ERROR';
-  } else {
-    if (lowEndParam > highEndParam) {
-      lowEnd = highEndParam;
-      highEnd = lowEndParam;
-    }
-
-    for (let i = lowEnd; i <= highEnd; i++) {
-      numList.push(i);
-    }
-
-    console.log(numList);
-    let sum = 0;
-    for (let j = 0; j < numList.length; j++) {
-      sum += numList[j];
-    }
-    console.log(sum);
-
-    return sum;
+  if (lowEndParam > highEndParam) {
+    lowEnd = highEndParam;
+    highEnd = lowEndParam;
   }
+
+  for (let i = lowEnd; i <= highEnd; i++) {
+    numList.push(i);
+  }
+
+  let sum = 0;
+  for (let j = 0; j < numList.length; j++) {
+    sum += numList[j];
+  }
+
+  return sum;
 };
 
 sumAll(1, 4);
