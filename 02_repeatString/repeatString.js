6db48f457d947a6eab92b843ca26687106da4873
Tommy White <tbw2.mail@gmail.com>
2022-02-23 18:30:04 -0700

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+    let i = 0;
+    while (i <= num || num === '') {
+      return str.repeat(num);
+    } i++
+  }
 };
 
 // Do not edit below this line
