c85f033b31a246646aff3c0cda729b46b9a19bd5
Tommy White <tbw2.mail@gmail.com>
2022-02-23 20:13:23 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitStr = str.split('');
+    let revArray = splitStr.reverse();
+    let joinArray = revArray.join('');
+    return joinArray;
 };
 
+
+reverseString('hello');
 // Do not edit below this line
 module.exports = reverseString;
