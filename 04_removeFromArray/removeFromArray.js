aa2dbed51ab8b001f27c276cb0386a134e1d0338
Tommy White <tbw2.mail@gmail.com>
2022-02-24 12:46:08 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+  let i = 0;
+  while (i < array.length) {
+    let j = 0;
+    while (j < nums.length) {
+      if (array[i] === nums[j]) {
+        array.splice(i, 1);
+        i--;
 
+      }
+      j++;
+
+    }
+    i++;
+
+} return array;
 };
 
 // Do not edit below this line
