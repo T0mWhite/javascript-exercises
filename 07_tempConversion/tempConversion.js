aa9153febf542322648327d5eb6001075754ced9
Tommy White <tbw2.mail@gmail.com>
2022-02-24 17:29:19 -0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cels = (far - 32) * (5/9);
+  cels = Math.round(cels * 10) / 10;
+  return cels;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let far = cels * (9/5) + 32;
+  far = Math.round(far * 10) / 10;
+  return far;
 };
+
 
 // Do not edit below this line
 module.exports = {
